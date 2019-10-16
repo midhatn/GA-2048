627c60d65a3bc583769fa61cc4c7f5fd759e6eb2
Midhat Nashar <medhat.nashar@gmail.com>
2019-10-16 18:21:03 +0300

    V3.0 incomplete

--- main1.js
+++ main1.js
@@ -5,132 +5,156 @@
 //     [2,2,2,2]
 // ]
 
-// for (var i=0; i<4; i++) {
-//     for (var j=0; j<4; j++) {
-//         $("div.box-topLeft").html(board[i][j] = i*j);
-//     }
-//  }
-
 
 /////// creating 4x4 boxes for game
 let z = 4;
-for (let i=0; i<z;i++){
-    for (let j=0; j<z; j++){
-$("div.container").append('<div class="box-'+i+j+'"></div>');
-////////appending 
-$("div.box-"+i+j+"").css({
-    "background-color":"#333",
-        "padding":"20px",
-        "border-radius":"10px",
-        "color":"#fff",
-        "display":"flex",
-        "align-items":"center",
-        "justify-content":"center",
-        "font-size":"40px",
-        "font-family":"sans-serif" 
-    })
-}
+for (let i = 0; i < z; i++) {
+    for (let j = 0; j < z; j++) {
+        $("div.container").append('<div class="box-' + i + j + '"></div>');
+        $("div.box-" + i + j + "").css({
+            "background-color": "#333",
+            "padding": "20px",
+            "border-radius": "10px",
+            "color": "#fff",
+            "display": "flex",
+            "align-items": "center",
+            "justify-content": "center",
+            "font-size": "40px",
+            "font-family": "sans-serif"
+        })
+    }
 }
 ///////random numbers generation
-function random2 (){
-    let rand1 = Math.floor(Math.random() * z)
-    let rand2 = Math.floor(Math.random() * z)
-   
-    if ($("div.box-"+rand1+rand2+"").html() <= 2){
-        $("div.box-"+rand1+rand2+"").html(2)
-    } 
-}
-random2()
-random2()
+// function random2 (){
+//     let rand1 = Math.floor(Math.random() * z)
+//     let rand2 = Math.floor(Math.random() * z)
+
+//     if ($("div.box-"+rand1+rand2+"").html() < 2){
+//         $("div.box-"+rand1+rand2+"").html(2)
+//     } 
+// }
+// random2()
+// random2()
 ///////////// 4x4 array
 
 /////////// pushing divs into array
- 
-
+$("div.box-" + 0 + 0 + "").html(2)
+$("div.box-" + 0 + 1 + "").html(2)
+$("div.box-" + 0 + 2 + "").html(2)
+$("div.box-" + 0 + 3 + "").html(2)
 // console.log(board)
+
+
 var board = [];
-for(var i=0; i<z; i++) {
+for (var i = 0; i < z; i++) {
     board[i] = [];
-    for(var j=0; j<z; j++) {
-        board[i][j] = undefined;
+    for (var j = 0; j < z; j++) {
+        board[i][j] = 0;
     }
 }
 
 
-let zeta = math.matrix(board)
-console.log(zeta)
-
 function pushToBoard() {
-for (let i=0; i<z;i++){
-    for (let j=0; j<z; j++){
-  board[i][j] = Number($("div.box-"+i+j+"").html())
-  
+    for (let i = 0; i < z; i++) {
+        for (let j = 0; j < z; j++) {
+            board[i][j] = Number($("div.box-" + i + j + "").html())
+
         }
     }
 }
 
+
 pushToBoard()
 
-// // let rand_x = Math.floor(Math.random() * 2)
-// // let rand_y = Math.floor(Math.random() * 2)
-// // board[rand_x][rand_y] = 2
-// // $("div.box-topLeft").html(board[0][0])
-// // $("div.box-topRight").html(board[0][1])
-// // $("div.box-bottomLeft").html(board[1][0])
-// // $("div.box-bottomRight").html(board[1][1])
-// score
-function scoreboard (){
-var score = 0;
-for (var i = 0; i < board.length; i++) {
-    for (var j = 0; j < board.length; j++) {
-    score += board[i][j] << 0;
-  
 
-}}
+function scoreboard() {
+    var score = 0;
+    for (var i = 0; i < board.length; i++) {
+        for (var j = 0; j < board.length; j++) {
+            score += board[i][j] << 0;
 
-$("div.box-score").html("Score: "+score)}
-// // $("div.box-topLeft").html()
-// // $("div.box-topRight").html()
-// // $("div.box-bottomLeft").html()
-// // $("div.box-bottomRight").html()
-// let topLeft = Number($("div.box-topLeft").html())
-// let topRight = Number($("div.box-topRight").html())
-// let bottomLeft = Number($("div.box-bottomLeft").html())
-// let bottomRight = Number($("div.box-bottomRight").html())
-function command (){
-        random2()
-        scoreboard()
-        pushToBoard()
+
+        }
+    }
+score = score+2
+    $("div.box-score").html("Score: " + score)
 }
 
-$(document).ready(function(){
-$("body").keydown(function(key){
-   
-    //left
-    if (key.which == 37) { 
-       
-        command ()
+function filter(){
+    var filtered0 = board[0].filter(Boolean);
+      var filtered1 = board[1].filter(Boolean);
+      var filtered2 = board[2].filter(Boolean);
+      var filtered3 = board[3].filter(Boolean);
+      board = []
     
+      board.push(filtered0);
+      board.push(filtered1);
+      board.push(filtered2);
+      board.push(filtered3);
+      }
 
-    }
-    //right
-    if (key.which == 39) {
-        
-       
-        command ()
-    }
-    //up
-    if (key.which == 38) {
-       
-        command ()
-        
-    }
-    //down
-	if (key.which == 40) {
-        command ()
+function command() {
+    random2()
+    scoreboard()
+    pushToBoard()
+}
 
-    } 
-    
+
+      console.log(board)
+$(document).ready(function () {
+    $("body").keydown(function (key) {
+        //left
+        if (key.which == 37) {
+            
+            for (i = 0; i < z; i++) {
+                for (j = 0; j < z; j++) {
+                    if ((board[i][j] == board[i][j + 1]) || (board[i][j] < 2)) {
+                        // console.log(board[i][j])
+                        // console.log(board[i][j + 1])
+                        filter()
+                        board[i][j] = board[i][j] + board[i][j + 1]
+                        filter()
+                        board[i][j + 1] = board[i][j + 2]
+                        filter()
+                        // board[i][j] = Number("")
+                        $("div.box-" + i + j + "").html(board[i][j])
+                        filter()
+                        $("div.box-" + i + (j + 1) + "").html(0)
+                        filter()
+                    }
+                    
+
+                }
+                
+
+            }
+            
+            
+           
+
+        }
+          filter()
+          scoreboard()
+          pushToBoard()
+          console.log(board)
+        
+        //right
+        if (key.which == 39) {
+
+
+
+        }
+        //up
+        if (key.which == 38) {
+
+
+        }
+        //down
+        if (key.which == 40) {
+            
+
+        }
+
     })
 
 })
